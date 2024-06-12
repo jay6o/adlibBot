dae4999b236fd63842c4f6811fc4041fe3f69283
Jonathan Urias <ih8merta@gmail.com>
2024-06-12 15:06:07 -0400

    removed unnecessary dependencies

--- start.py
+++ start.py
@@ -2,17 +2,13 @@ from selenium import webdriver
 from modules.input_search import input_search
 from modules.filters import filters
 from modules.scroll import scroll
-import threading
-from pynput import keyboard
 
 def start():  
   search_terms = input("Enter your search terms: ")
   driver = webdriver.Chrome()
   driver.get("https://www.facebook.com/ads/library/")
-  t1 = threading.Thread()
   input_search(driver, search_terms)
   filters(driver)
-  global stop_scrolling
   stop_scrolling = False
   scroll(driver, stop_scrolling)
   driver.quit()
